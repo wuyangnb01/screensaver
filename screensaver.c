89fa14170714c6c639097e74e276cbcaa08bb578
2025009589-Wu Yang <1776383590@qq.com>
2026-07-14 14:45:47 +0800

    the second which adds the key but not realize the functions

--- screensaver.c
+++ screensaver.c
@@ -16,6 +16,7 @@ static const char *names[] = {
 };
 
 #define FPS 1
+static int speed_draw = 1;
 
 static inline uint8_t R(uint32_t p) { return p >> 16; }//add inline keyword can unfold the function in there
 static inline uint8_t G(uint32_t p) { return p >> 8; }
@@ -44,7 +45,7 @@ int check_key() {
     }
     if (ev.keydown && ev.keycode != AM_KEY_ESCAPE) {
         printf("Got key: %s (%d)\n", names[ev.keycode], ev.keycode);
-        speed_factor = 10;
+        speed_draw = 10;
         return 1;
     }
     return 0;
@@ -53,82 +54,62 @@ int check_key() {
 void draw_color() {
     buffer = (uint32_t *)malloc(SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(uint32_t));
     
-    
-    if(flag_qk) 
-    {
-        unsigned long last = 0;
-    }else {
-        unsigned long last = 200;
-    }
+    unsigned long last = 0;
     unsigned long fps_last = 0;
     int fps = 0;
+    uint32_t current_color = color[0];
     
-
     while (1) {
-        unsigned long upt = io_read(AM_TIMER_UPTIME).us / 1000;//convert to ms
-        uint32_t init_color = color[0];
-        uint32_t target_color = color[rand() % 8];
-        uint32_t color_buf[8];
-        int i=0,k=0;
-        for (i = 0; i < 8; i++) {
-            if (color[i] == target_color) {
-                k=i;
-                break;
+        unsigned long upt = io_read(AM_TIMER_UPTIME).us / 1000;
+        uint32_t target_color = color[rand() % 7 + 1];
+        int key_ret = check_key();
+        if (key_ret == -1) {
+            free(buffer);
+            return;
+        }
+        
+        if (upt - last > 1000 / (FPS * speed_draw)) {
+            int k=0,i=0;
+            for(i=1;i<8;i++)
+            {
+                if(target_color == color[i])
+                {
+                    k=i;
+                    break;
+                }
             }
+            for (int i = 1; i <= k; i++) {
+                uint32_t r = R(current_color) + i * (R(target_color) - R(current_color)) / k;
+                uint32_t g = G(current_color) + i * (G(target_color) - G(current_color)) / k;
+                uint32_t b = B(current_color) + i * (B(target_color) - B(current_color)) / k;
+                draw(pixel(r, g, b));
+            }
+            
+            last = upt;
+            fps++;
         }
-        for(i=1;i<k;i++){
-            color_buf[i]=pixel(R(init_color)+i*(R(target_color)-R(init_color))/k,
-                            G(init_color)+i*(G(target_color)-G(init_color))/k,
-                            B(init_color)+i*(B(target_color)-B(init_color))/k);
-           if(upt - last > 1000 / FPS) {
-                draw(color_buf[i]);
-                last = upt;
-                fps ++;
-           }
-           if(upt - fps_last > 1000) {// ensure 1s display fps only once
-                // display fps every 1s
-                printf("fps: %d\n", fps);
-                fps_last = upt;
-                fps = 0;
-           }
+        
+        if (upt - fps_last > 1000) {
+            printf("fps: %d, speed: %dx\n", fps, speed_draw);
+            fps_last = upt;
+            fps = 0;
+            current_color = target_color;
         }
-    }
-    free(buffer);
-}
-
-
-
-int keys() {
-    AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-    if (ev.keycode == AM_KEY_NONE || ev.keycode == AM_KEY_ESCAPE) return 0;
-    printf("Got  (kbd): %s (%d) %s\n", names[ev.keycode], ev.keycode, ev.keydown ? "DOWN" : "UP");
-    if(ev.keydown && ev.keycode != AM_KEY_ESCAPE) {
-        draw_color(1);
-        return 1;
+        if (speed_draw > 1 && upt - last > 5000) {
+            speed_draw = 1; 
+            printf("Speed restored to normal\n");
+        }   
+        yield();
     }
 }
-
-void key() {
-    printf("Try to press  key \n");
-    printf("If you press ESC ,this process will exit.\n");
-    printf("And when you press any keys ,you should see the change be quickened.\n");
-    while (1) {
-      int ret = keys();
-      if(ret) break;
-      else {
-        return;
-      }
-      yield();
-    }
-}
-
-
 
 int main() {
     srand(time(NULL));
     ioe_init();
-    key();
-    draw_color(0);
+    printf("Screen saver started.\n");
+    printf("Press any key to speed up, ESC to exit.\n");
+    draw_color();
+    free(buffer);
     yield();
     return 0;
 }
